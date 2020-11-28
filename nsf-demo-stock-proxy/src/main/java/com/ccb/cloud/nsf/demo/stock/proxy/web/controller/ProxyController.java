d1c125ebe3f74f256c624195025f79af3856255e
陈佳翰 <chenjiahan@corp.netease.com>
2020-11-28 22:23:34 +0800

    proxy透传header

--- nsf-demo-stock-proxy/src/main/java/com/ccb/cloud/nsf/demo/stock/proxy/web/controller/ProxyController.java
+++ nsf-demo-stock-proxy/src/main/java/com/ccb/cloud/nsf/demo/stock/proxy/web/controller/ProxyController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +18,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.net.URI;
 import java.nio.charset.Charset;
+import java.util.Enumeration;
 
 @RestController
 @ResponseBody
@@ -35,12 +37,21 @@ public class ProxyController {
         String uri = request.getRequestURI();
         String queryString = request.getQueryString();
         String method = request.getMethod();
+
+        Enumeration<String> headerNames = request.getHeaderNames();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerVal = request.getHeader(headerName);
+            httpHeaders.add(headerName, headerVal);
+        }
+
         String body = StreamUtils.copyToString(request.getInputStream(), Charset.forName("UTF-8"));
         URI url = URI.create(proxy + uri + "?" +queryString);
 
         logger.info("proxy, url:{}", url);
 
-        ResponseEntity<String> response = restTemplate.exchange(new RequestEntity<>(body, HttpMethod.resolve(method), url), String.class);
+        ResponseEntity<String> response = restTemplate.exchange(new RequestEntity<>(body, httpHeaders,HttpMethod.resolve(method), url), String.class);
         if (response.getStatusCodeValue() == 200) {
             return response.getBody();
         }
